8a8b31253c85e81cdc40f0637d8b705dfed3df73
Cory Johns <johnsca@gmail.com>
2016-02-29 19:07:14 -0500

    Refactors and fixes based on reviews

--- reactive/flume_hdfs.py
+++ reactive/flume_hdfs.py
@@ -1,8 +1,7 @@
 from charms.reactive import when, when_not
-from charms.reactive import set_state, remove_state
+from charms.reactive import set_state, remove_state, is_state
 from charmhelpers.core import hookenv
-from charms.flume import Flume
-from charms.hadoop import get_dist_config
+from charms.layer.apache_flume_base import Flume
 from charms.reactive.helpers import any_file_changed
 
 
@@ -13,22 +12,25 @@ def report_unconnected():
 
 @when('hadoop.related')
 @when_not('hadoop.hdfs.ready')
-def report_waiting():
+def report_waiting(hadoop):  # pylint: disable=unused-argument
     hookenv.status_set('waiting', 'Waiting for HDFS')
 
 
-@when('flume-agent.joined')
-def sending_connection_info_to_agent(flume_agent):
+@when('flume-source.joined')
+def sending_connection_info_to_agent(source):
     config = hookenv.config()
-    flume_agent.send_configuration(config['source_port'], config['protocol'])
+    source.send_configuration(config['source_port'], config['protocol'])
 
 
 @when('flume-base.installed', 'hadoop.hdfs.ready')
 def configure_flume(hdfs):  # pylint: disable=unused-argument
     hookenv.status_set('maintenance', 'Configuring Flume')
-    flume = Flume(get_dist_config())
+    flume = Flume()
     flume.configure_flume()
-    if any_file_changed(flume.config_file):
+    if not is_state('flume-hdfs.hdfs.inited'):
+        flume.init_hdfs()
+        set_state('flume-hdfs.hdfs.inited')
+    if any_file_changed([flume.config_file]):
         flume.restart()
     set_state('flume-hdfs.started')
     hookenv.status_set('active', 'Ready')
@@ -37,6 +39,6 @@ def configure_flume(hdfs):  # pylint: disable=unused-argument
 @when('flume-hdfs.started')
 @when_not('hadoop.ready')
 def stop_flume():
-    flume = Flume(get_dist_config())
+    flume = Flume()
     flume.stop()
     remove_state('flume-hdfs.started')
